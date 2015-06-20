6e33c5303e4f7970d3dd5f98d3f95499f6ba8f90
Simon Colmer <simon_colmer@hotmail.com>
2015-06-21 00:57:35 +0100

    Added IisConfig

---
+++ src/TeamCityView.IisConfig/Program.cs
@@ -0,0 +1,15 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityView.IisConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
