76ec05dfa3eddbc5e83d3332856f6a92051781a0
Simon Colmer <simon_colmer@hotmail.com>
2015-06-21 14:01:24 +0100

    Can now detect builds for a given user within 24 hours

--- src/TeamCityView.Domain/TeamServer.cs
+++ src/TeamCityView.Domain/TeamServer.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using TeamCitySharp;
 using TeamCitySharp.DomainEntities;
+using TeamCitySharp.Locators;
 
 namespace TeamCityView.Domain
 {
     public class TeamServer : ITeamServer
     {
         private readonly TeamCityClient _client;
-        //private TimeSpan _detectsBuildsFrom = TimeSpan.FromDays(500); //TimeSpan.FromDays(1);
+        private readonly TimeSpan _detectBuildsFrom = TimeSpan.FromDays(1);//TimeSpan.FromDays(500); //TimeSpan.FromDays(1);
 
         //"http://es-ci-server.cloudapp.net/"
 
@@ -27,7 +28,7 @@ namespace TeamCityView.Domain
         public Build[] ListBuildStatusesForTeam(string teamName)
         {
             List<User> users = _client.Users.AllUsersByUserGroup(teamName);
-            List<Build> builds = ListLatestBuildsForUsersInLast24Hours(users);
+            List<Build> builds = ListLatestBuildsForUsersInLast24Hours(users);//ListLatestBuildsForUsers(users);
 
             return builds
                 .OrderByDescending(x => x.StartDate)
@@ -35,13 +36,32 @@ namespace TeamCityView.Domain
                 .ToArray();
         }
 
-        private List<Build> ListLatestBuildsForUsersInLast24Hours(List<User> users)
+        private List<Build> ListLatestBuildsForUsers(List<User> users)
         {
             var result = new List<Build>();
 
             foreach (User user in users)
             {
                 result.AddRange(_client.Builds.ByUserName(user.Username));
+            }
+
+            return result;
+        }
+
+        private List<Build> ListLatestBuildsForUsersInLast24Hours(List<User> users)
+        {
+            var result = new List<Build>();
+            const int maxResults = 20;
+            DateTime fromDate = DateTime.Now.Add(-_detectBuildsFrom).Date;
+
+            foreach (User user in users)
+            {
+                var builds = _client.Builds.ByBuildLocator(BuildLocator.WithDimensions(null, UserLocator.WithUserName(user.Username),
+                    agentName: null, status: null, personal: null, canceled: null, running: null, pinned: null,
+                    maxResults: maxResults, startIndex: null, sinceBuild: null, sinceDate: fromDate, tags: null,
+                    branch: null));
+
+                result.AddRange(builds);
             }
 
             return result;
